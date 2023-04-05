c184a85016b2a3ca2fca69c5f119221dc064bc7f
Eldorado <eldorado.mukaj@gmail.com>
2023-04-05 04:29:09 -0400

    Add export to package.json

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { FiArrowDown } from 'react-icons/fi';
-import { VscGithubInverted } from 'react-icons/vsc';
-import { Button } from './Button';
-import Lottie from 'react-lottie';
-import animationData from '../../public/assets/computer.json';
+import React from "react"
+import { FiArrowDown } from "react-icons/fi";
+import { VscGithubInverted } from "react-icons/vsc";
+import { Button } from "./Button";
+import Lottie from "react-lottie";
+import animationData from "../../public/assets/computer.json";
 
 function Main() {
   const defaultOptions = {
@@ -15,25 +15,25 @@ function Main() {
     }
   }
   return (
-    <main className='w-full h-screen flex justify-center items-center'>
-        <div className='flex flex-col items-center content-center align-middle'>
-            <div className='mb-0'>
-                {/* <img className='bg-red-500 rounded-full' src="https://robohash.org/rgdbdh5hy5h5h" /> */}
+    <main className="w-full h-screen flex justify-center items-center">
+        <div className="flex flex-col items-center content-center align-middle">
+            <div className="mb-0">
+                {/* <img className="bg-red-500 rounded-full" src="https://robohash.org/rgdbdh5hy5h5h" /> */}
                 <Lottie options={defaultOptions} isClickToPauseDisabled={true} />
             </div>
 
-            <h1 className=''>Hi, I'm { }
-                <span className='text-[#0FFF95]'>Eldorado</span>
+            <h1>Hi, I&#8217;m { }
+                <span className="text-[#0FFF95]">Eldorado</span>
             </h1>
 
-            <h3 className='mt-4 lowercase text-[#3a4d97] font-terminal font-extralight tracking-wide'>Systems Engineer | Developer | Homelab Enthusiast</h3>
+            <h3 className="mt-4 lowercase text-[#3a4d97] font-terminal font-extralight tracking-wide">Systems Engineer | Developer | Homelab Enthusiast</h3>
 
-            <p className=' max-w-lg mt-8 px-8 sm:px-4 text-justify'>Welcome to my portfolio website! I am a Systems Engineer, Developer, and Homelab Enthusiast with a passion for designing and implementing complex systems, building software applications, and tinkering with technology. </p>
+            <p className="max-w-lg mt-8 px-8 sm:px-4 text-justify">Welcome to my portfolio website! I am a Systems Engineer, Developer, and Homelab Enthusiast with a passion for designing and implementing complex systems, building software applications, and tinkering with technology. </p>
         
-            <a href="https://github.com/eldoradomukaj" target='_blank'><Button text="Github" icon={<VscGithubInverted size={34} />} /></a> 
+            <a href="https://github.com/eldoradomukaj" target="_blank"><Button text="Github" icon={<VscGithubInverted size={34} />} /></a> 
         </div>
 
-        {/* <span className='text-3xl p-2 absolute bottom-0 mb-8 animate-bounce cursor-pointer'><FiArrowDown /></span> */}
+        {/* <span className="text-3xl p-2 absolute bottom-0 mb-8 animate-bounce cursor-pointer"><FiArrowDown /></span> */}
     </main>
   )
 }
