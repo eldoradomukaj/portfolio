01ef76fdbb8321cb93bfde4fb9882c4f025bcf65
Eldorado <eldorado.mukaj@gmail.com>
2023-04-05 12:12:24 -0400

    Updated site title

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Eldorado Mukaj</title>
+        <title>Eldorado Mukaj | Portfolio</title>
         <meta name="description" content="Eldorado Mukaj portfolio website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
